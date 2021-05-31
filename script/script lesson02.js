560fa4bf6707ced458bf6f7e8f4e6f0a9707834a
Danil Medvedev <danilmedvedev26@mail.ru>
2021-05-31 22:57:11 +0300

    Добавил исходник  к уроку02

---
+++ script/script lesson02.js
@@ -0,0 +1,29 @@
+/* Указываю именно переменную let (а не var), т.к. в первой лекции слышал, что она более современне чем var */
+
+let money = 450000; 
+console.log(typeof money);
+let income = "freelance";
+console.log(typeof income);
+let addExpenses = "Flatpay, Apartment-receipt, Metro";
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split (" "))
+
+let deposit = false; 
+console.log(typeof deposit);
+let mission = 1; 
+let period = 8;
+console.log("Период равен " + period + " месяцев." + " Цель заработать " + mission + " рублей.");
+
+
+let budgetDay = 450000/30; 
+console.log(budgetDay)
+
+
+
+
+
+
+
+
+
